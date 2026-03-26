c85cd21e258d2dd2b3772c054a43a73e57403341
Ayansa Adugna <zoopaad@gmail.com>
2026-03-26 18:19:08 +0300

    Initial commit: Spring Boot project setup with dependencies and configuration

---
+++ src/main/java/com/shopwave/shopwave_starter/ShopwaveStarterApplication.java
@@ -0,0 +1,23 @@
+package com.shopwave.shopwave_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootApplication
+public class ShopwaveStarterApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopwaveStarterApplication.class, args);
+	}
+
+
+
+
+
+}
---
+++ src/test/java/com/shopwave/shopwave_starter/ShopwaveStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopwave.shopwave_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwaveStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
